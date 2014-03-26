b4bfb757daf06276744bd053bd469dc8b401ec15
dancifensy <line2up2003@aliyun.com>
2014-03-26 20:54:59 +0800

    蓝牙改为5秒钟，解决有时连接失败的情况。

--- ti/Projects/ble/Health/Source/simpleBLEPeripheral.c
+++ ti/Projects/ble/Health/Source/simpleBLEPeripheral.c
@@ -90,7 +90,7 @@
 #define SBP_PERIODIC_EVT_PERIOD               5000
 
 // What is the advertising interval when device is discoverable (units of 625us, 160=100ms)
-#define DEFAULT_ADVERTISING_INTERVAL          1600//0
+#define DEFAULT_ADVERTISING_INTERVAL          8000//1600//0
 
 // Limited discoverable mode advertises for 30.72s, and then stops
 // General discoverable mode advertises indefinitely
